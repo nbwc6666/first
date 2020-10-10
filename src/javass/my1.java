7ceea9a14efadfb10e6629c6eaaeb5d30950b898
Administrator <Administrator@UGO>
2020-10-10 10:50:18 +0800

    我第四次dnmd

--- src/javass/my1.java
+++ src/javass/my1.java
@@ -4,6 +4,8 @@ public class my1{
 	public static void main(String[] args) {
 		System.out.println("wdnmd");
 		System.out.println("我第二次dnmd");
+		System.out.println("我第三次dnmd");
+		System.out.println("我第四次dnmd");
 	}
 
 }
